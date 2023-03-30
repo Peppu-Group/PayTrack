163bced15d1fc63433c324c17786c8378496d100
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-03-30 12:58:37 +0100

    open new sheet in tab

--- Code.js
+++ Code.js
@@ -349,6 +349,25 @@ function copyFile(e) {
     .build() && createFile();
 }
 
+function openUrl( url ){
+  var html = HtmlService.createHtmlOutput('<!DOCTYPE html><html><script>'
+  +'window.close = function(){window.setTimeout(function(){google.script.host.close()},9)};'
+  +'var a = document.createElement("a"); a.href="'+url+'"; a.target="_blank";'
+  +'if(document.createEvent){'
+  +'  var event=document.createEvent("MouseEvents");'
+  +'  if(navigator.userAgent.toLowerCase().indexOf("firefox")>-1){window.document.body.append(a)}'                          
+  +'  event.initEvent("click",true,true); a.dispatchEvent(event);'
+  +'}else{ a.click() }'
+  +'close();'
+  +'</script>'
+  // Offer URL as clickable link in case above code fails.
+  +'<body style="word-break:break-word;font-family:sans-serif;">Failed to open automatically.  Click below:<br/><a href="'+url+'" target="_blank" onclick="window.close()">Click here to proceed</a>.</body>'
+  +'<script>google.script.host.setHeight(55);google.script.host.setWidth(410)</script>'
+  +'</html>')
+  .setWidth( 90 ).setHeight( 1 );
+  SpreadsheetApp.getUi().showModalDialog( html, "Opening ..." );
+}
+
 function viewInvoice(e) {
   var res = e['formInput'];
   var contactName = res['Contact Name'] ? res['Contact Name'] : '';
